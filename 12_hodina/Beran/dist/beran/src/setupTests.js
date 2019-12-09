a632035bf5266489efe2003c7abd8feedb864992
Beran Vitezslav :-) <BeranVi>
2019-12-09 15:50:29 +0100

    obrázek

---
+++ 12_hodina/Beran/dist/beran/src/setupTests.js
@@ -0,0 +1,13 @@
+System.register(["@testing-library/jest-dom/extend-expect"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (_1) {
+            }
+        ],
+        execute: function () {
+        }
+    };
+});
+//# sourceMappingURL=setupTests.js.map
