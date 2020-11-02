2cdf458f16d2e41a342ebb1f6cbf7618a9ce635a
Tomáš Kalina <info@tomaskalina.cz>
2020-11-02 15:55:54 +0100

    pridani materialu

---
+++ 3_hodina/index.js
@@ -0,0 +1,19 @@
+//podminky
+if ((1 === 2) && (2 === 2) && (3 === 2)) {
+    console.log("ano");
+}
+else if (3===4) {
+    console.log("ne");
+}
+
+
+//cykly
+let pole = ["ahoj", "cau", "nazdar"];
+
+console.log(pole.length);
+
+for (let c = 0; c < pole.length; c++) {
+
+    console.log(pole[c])
+
+}
